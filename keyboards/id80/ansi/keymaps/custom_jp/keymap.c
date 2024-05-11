b8c7a86b213c09e28507208c929fafbf10053e58
lowbrain <83059975+lowbrain@users.noreply.github.com>
2024-05-11 19:28:02 +0900

    DEL<->PRINT

--- keyboards/id80/ansi/keymaps/custom_jp/keymap.c
+++ keyboards/id80/ansi/keymaps/custom_jp/keymap.c
@@ -25,19 +25,19 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEF] = LAYOUT(
-        KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_PSCR,    KC_DEL,
+        KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,     KC_PSCR,
         JU_GRV,  KC_1,    JU_2,    KC_3,    KC_4,    KC_5,    JU_6,    JU_7,    JU_8,    JU_9,    JU_0,    JU_MINS, JU_EQL,  KC_BSPC,            KC_HOME,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JU_LBRC, JU_RBRC, JU_BSLS,        KC_END,
         JU_CAPS,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    JU_SCLN, JU_QUOT, KC_ENT,
-        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_FN0),         KC_UP,
-        KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              KC_APP,      KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,         KC_UP,
+        KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                         LT(_FN0, KC_APP), KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FN0] = LAYOUT(
-        RESET,   MO(_FN1), MO(_FN2), MO(_FN3), MO(_FN4), _______, _______, _______, _______,   _______, _______, _______, _______,   KC_SLCK,    KC_INS,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_DEL,            KC_PGUP,
-        _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        KC_PGDN,
-        _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        KC_MS_WH_UP,
+        RESET,   MO(_FN1), MO(_FN2), MO(_FN3), MO(_FN4), _______, _______, _______, _______,   _______, _______, _______, _______,    _______,   _______,
+        _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,            KC_PGUP,
+        _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_HOME,  KC_END, _______,        KC_PGDN,
+        _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
+        _______,            _______, _______, _______, _______, _______, _______, _______, _______,  KC_INS,  KC_DEL, _______,        KC_MS_WH_UP,
         _______,   _______,   _______,                       _______,                             _______,     _______, KC_MS_WH_LEFT,KC_MS_WH_DOWN,KC_MS_WH_RIGHT
     ),
     [_FN1] = LAYOUT(
